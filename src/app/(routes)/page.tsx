94de45a2823b836c5460d68269c06e0dd5b3b3cd
José <98621042+joseiedo@users.noreply.github.com>
2023-09-15 02:06:22 +0000

    feat: add `card` component

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -2,12 +2,25 @@
 import { Button } from "@/components";
 import Header from "@/components/helpers/Header.component";
 import Hero from "./hero";
+import Card from "@/components/helpers/Card.component";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Card
+        imagePath="/posso-doar-sangue.jpg"
+        imageAlt="Homem doando sangue"
+        heading="Posso doar sangue?"
+        description="Veja se você pode doar sangue respondendo breves perguntas"
+        link={
+          {
+            href: "./",
+            label: "Faça o teste de elegibilidade"
+          }
+        }
+      />
     </>
   );
 }
