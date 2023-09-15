253f8b7bd646cd950bf406c8d6c11bbd4f64f72b
José <98621042+joseiedo@users.noreply.github.com>
2023-09-15 01:41:06 +0000

    feat: add home `hero`

---
+++ src/app/(routes)/hero.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className="relative bg-pink-600 ">
+      <Image
+        className="w-full object-cover block max-h-96"
+        src={"/hero-bg.jpg"}
+        width={1280}
+        alt=""
+        height={397}
+      />
+      <div className="absolute h-full w-full top-0 flex items-center justify-center">
+        <h2 className="text-4xl font-medium text-white">
+          Uma única doação <br />
+          pode salvar até <br />
+          <span className="bg-rose-500">
+            Quatro vidas.
+          </span>
+        </h2>
+      </div>
+    </div >
+  );
+}
--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 import { Button } from "@/components";
 import Header from "@/components/helpers/Header.component";
+import Hero from "./hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-2xl font-bold">Hello, world!</h1>
-        <Button onChange={() => console.log("Teste")} text="Teste" />
-      </main>
+      <Hero />
     </>
   );
 }
