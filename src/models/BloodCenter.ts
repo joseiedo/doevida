1bb6ec5d383ae1aa615a327ed5b859278d13e4ed
José Iêdo <iedoduarte1@gmail.com>
2023-12-09 12:27:25 -0300

    docs: add jsdoc into Bloodcenter model functions, add .nvmrc

--- src/models/BloodCenter.ts
+++ src/models/BloodCenter.ts
@@ -8,16 +8,32 @@ import { getDBClient } from "@/infra/database";
 const infoPattern = /(Endereço|CEP|Telefone|E-mail):?/gi;
 const BLOODCENTER_WEBSITE_URL = process.env.BLOODCENTER_WEBSITE_URL;
 
+/**
+ * Caches the data of blood centers in the database.
+ * @param regions - An array of regions containing blood center data.
+ * @returns A Promise that resolves when the data is successfully cached.
+ */
 async function cacheData(regions: IBloodCenterRegion[]) {
   const db = await getDBClient();
   db?.collection("bloodcenters").insertMany(regions);
 }
+
+/**
+ * Retrieves cached data of blood centers from the database.
+ * @returns {Promise<IBloodCenterRegion[]>} A promise that resolves to an array of blood center data grouped by region.
+ */
 async function getCachedData(): Promise<IBloodCenterRegion[]> {
   const db = await getDBClient();
   const bloodcenters = await db?.collection("bloodcenters").find({}).toArray();
 
   return JSON.parse(JSON.stringify(bloodcenters)) as IBloodCenterRegion[];
 }
+
+/**
+ * Map all info of bloodcenter by type of information.
+ * @param contact - The contact information of the blood center returned in the scraping fase.
+ * @returns The formatted contact information as an object.
+ */
 function formatBloodCenterInfo(contact: string): IContact {
   const separator = "\n";
   const [address, postalCode, phone, email] = contact
@@ -35,6 +51,12 @@ function formatBloodCenterInfo(contact: string): IContact {
   };
 }
 
+/**
+ * Cleans the contact information of a blood center.
+ * @param content - The contact information to be cleaned.
+ * @returns The cleaned contact information.
+ * @throws Error if the contact information does not match any known type.
+ */
 function cleanBloodCenterContactInfo(content: string): IContactInfo {
   const contactsTypes: { [key: string]: IContactInfoType } = {
     Endereço: "ADDRESS",
@@ -50,7 +72,7 @@ function cleanBloodCenterContactInfo(content: string): IContactInfo {
   );
 
   if (!matchedType) {
-    throw Error;
+    throw Error(`No matched type for information "${content}"`);
   }
 
   return {
@@ -59,6 +81,12 @@ function cleanBloodCenterContactInfo(content: string): IContactInfo {
   };
 }
 
+/**
+ * Retrieves all blood centers.
+ * If there is cached data available, it returns the cached data.
+ * Otherwise, it scrapes the data and returns it.
+ * @returns {Promise<IBloodCenterRegion[]>} A promise that resolves to an array of blood centers grouped by region.
+ */
 async function getAll(): Promise<IBloodCenterRegion[]> {
   const cachedData = await getCachedData();
 
@@ -69,6 +97,11 @@ async function getAll(): Promise<IBloodCenterRegion[]> {
   }
 }
 
+/**
+ * Scrapes data from government bloodcenters locations website.
+ * @returns {Promise<IBloodCenterRegion[]>} A promise of the scraped data containing regions and blood centers.
+ * @throws {Error} If the BLOODCENTER_WEBSITE_URL environment variable is empty.
+ */
 async function scrapData(): Promise<IBloodCenterRegion[]> {
   if (BLOODCENTER_WEBSITE_URL === undefined) {
     throw Error("Environment variable BLOODCENTER_WEBSITE_URL is empty");
